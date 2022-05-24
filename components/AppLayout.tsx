6c56cf73c57b7237c329a8deaed42f1537491b67
Eric Wang <eric.wang401@gmail.com>
2022-05-24 17:31:49 +0200

    Fix hydration error

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -13,15 +13,8 @@ import {
   XIcon,
 } from '@heroicons/react/outline'
 import Link from 'next/link'
+import { useUser } from '@auth0/nextjs-auth0'
 
-const navigation = [
-  { name: 'Dashboard', href: '/', icon: HomeIcon },
-  { name: 'Team', href: '/questions', icon: UsersIcon },
-]
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
-}
 
 interface AppLayoutProps {
   children: React.ReactNode
@@ -30,6 +23,20 @@ interface AppLayoutProps {
 const AppLayout = ({ children }: AppLayoutProps) => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
   const router = useRouter()
+  const { user } = useUser()
+
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+  if (!user) {
+    return <h1>Faild to fetch user info</h1>
+  }
+
+  const navigation = [
+    { name: 'Dashboard', href: '/', icon: HomeIcon },
+    { name: 'Team', href: '/questions', icon: UsersIcon },
+  ]
 
   return (
     <>
@@ -118,21 +125,24 @@ const AppLayout = ({ children }: AppLayoutProps) => {
                   </nav>
                 </div>
                 <div className="flex-shrink-0 flex bg-gray-700 p-4">
-                  <a href="#" className="flex-shrink-0 group block">
+                  <div className="flex-shrink-0 group block">
                     <div className="flex items-center">
                       <div>
                         <img
                           className="inline-block h-10 w-10 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          src={user.picture as string}
                           alt=""
                         />
                       </div>
                       <div className="ml-3">
-                        <p className="text-base font-medium text-white">Tom Cook</p>
-                        <p className="text-sm font-medium text-gray-400 group-hover:text-gray-300">View profile</p>
+                        <p className="text-base font-medium text-white">{ user.name }</p>
+
+                        <Link href="/api/auth/logout">
+                          <a className="text-sm font-medium text-gray-400 group-hover:text-gray-300">Sign out</a>
+                        </Link>
                       </div>
                     </div>
-                  </a>
+                  </div>
                 </div>
               </div>
             </Transition.Child>
@@ -177,23 +187,23 @@ const AppLayout = ({ children }: AppLayoutProps) => {
               </nav>
             </div>
             <div className="flex-shrink-0 flex bg-gray-700 p-4">
-              <a href="#" className="flex-shrink-0 w-full group block">
+              <div className="flex-shrink-0 w-full group block">
                 <div className="flex items-center">
                   <div>
                     <img
                       className="inline-block h-9 w-9 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      src={user.picture as string}
                       alt=""
                     />
                   </div>
                   <div className="ml-3">
-                    <p className="text-sm font-medium text-white">Tom Cook</p>
+                    <p className="text-sm font-medium text-white">{user.name}</p>
                     <Link href="/api/auth/logout">
                       <a className="text-xs font-medium text-gray-300 group-hover:text-gray-200">Sign out</a>
                     </Link>
                   </div>
                 </div>
-              </a>
+              </div>
             </div>
           </div>
         </div>
