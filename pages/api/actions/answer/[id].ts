beb53736d6eb077b2e387e803017200fb3bcb589
Eric Wang <eric.wang401@gmail.com>
2022-06-06 23:14:59 +0200

    Fix post defined multiple times

--- pages/api/actions/answer/[id].ts
+++ pages/api/actions/answer/[id].ts
@@ -9,19 +9,6 @@ const buzzIn = async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).end()
 }
 
-const post = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.body.type && req.body.type === 'trafficLightEvent') {
-    res.socket.server.io.sockets.emit('first-team-selected', req.query.id)
-    res.status(200).json({
-      message: 'First team selected and emitted',
-    })
-  }
-
-  res.socket.server.io.sockets.emit('select-team', req.query.id)
-
-  res.status(200).end()
-}
-
 const get = {
   method: 'GET' as const,
   handler: buzzIn,
@@ -29,7 +16,18 @@ const get = {
 
 const post = {
   method: 'POST' as const,
-  handler: post,
+  handler: async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.body.type && req.body.type === 'trafficLightEvent') {
+      res.socket.server.io.sockets.emit('first-team-selected', req.query.id)
+      res.status(200).json({
+        message: 'First team selected and emitted',
+      })
+    }
+
+    res.socket.server.io.sockets.emit('select-team', req.query.id)
+
+    res.status(200).end()
+  },
 }
 
 export default withProperMethods([get, post])
