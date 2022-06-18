31abe61e778ecc3af78b34880219eb0660a5d7a9
Eric Wang <eric.wang401@gmail.com>
2022-06-18 20:26:27 +0200

    stop showing answer on completion

--- components/GameContainer.tsx
+++ components/GameContainer.tsx
@@ -350,7 +350,7 @@ const GameContainer = ({ questions, teams, timeoutValue }: GameContainerProps) =
             </div>
             <div>
 
-                {!selectedTeamId && !isAcceptingAnswers && <div className='ml-5 mt-5 min-w-[30rem] absolute shadow sm:rounded-md sm:overflow-hidden bg-white px-4 py-5 sm:p-6'>
+                {!selectedTeamId && !completed && !isAcceptingAnswers && <div className='ml-5 mt-5 min-w-[30rem] absolute shadow sm:rounded-md sm:overflow-hidden bg-white px-4 py-5 sm:p-6'>
                     <h3 className='text-4xl font-bold'>Correct Answer</h3>
                     <p className='text-2xl'>{questions[selectedQuestion].correctAnswer ? questions[selectedQuestion].correctAnswer : 'No correct answers'}</p>
                 </div>}
