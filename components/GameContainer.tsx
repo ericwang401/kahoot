5ac9315b5249d1af2323952d821e6de68b45a635
Eric Wang <eric.wang401@gmail.com>
2022-06-23 18:52:33 +0200

    Fix show answer

--- components/GameContainer.tsx
+++ components/GameContainer.tsx
@@ -254,6 +254,7 @@ const GameContainer = ({ questions, teams, timeoutValue }: GameContainerProps) =
 
         timeoutRef.current = setTimeout(() => {
             setIsAcceptingAnswers(false)
+            setShowAnswer(true)
             console.log('no longer accepting answers')
             controls.set({
                 width: 0
@@ -391,7 +392,7 @@ const GameContainer = ({ questions, teams, timeoutValue }: GameContainerProps) =
             </div>
             <div>
 
-                {(!selectedTeamId && !completed && !isAcceptingAnswers || showAnswer) && <div className='ml-5 mt-5 min-w-[30rem] absolute shadow sm:rounded-md sm:overflow-hidden bg-white px-4 py-5 sm:p-6'>
+                {(!selectedTeamId && !completed && showAnswer) && <div className='ml-5 mt-5 min-w-[30rem] absolute shadow sm:rounded-md sm:overflow-hidden bg-white px-4 py-5 sm:p-6'>
                     <h3 className='text-4xl font-bold'>Correct Answer</h3>
                     <p className='text-2xl'>{questions[selectedQuestion].correctAnswer ? questions[selectedQuestion].correctAnswer : 'No correct answers'}</p>
 
